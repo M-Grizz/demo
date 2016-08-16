f59649346359fe0697587f1d4f7df176226327c7
L00C35开发三部 助理开发工程师 马高杉 <magsh@pycredit.cn>
2016-08-16 11:43:21 +0800

    初始化代码

---
+++ src/test/java/com/github/grizz/application/DemoApplication.java
@@ -0,0 +1,13 @@
+package com.github.grizz.application;
+
+import org.apache.commons.lang3.StringUtils;
+
+public class DemoApplication {
+
+	public static void main(String[] args) {
+		
+		System.out.println(StringUtils.isEmpty(""));
+		
+		
+	}
+}
---
+++ src/test/java/com/github/grizz/application/ExcelDemo.java
@@ -0,0 +1,10 @@
+package com.github.grizz.application;
+
+public class ExcelDemo {
+
+	public static void main(String[] args) {
+		
+
+	}
+
+}
